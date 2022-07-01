462c4236a1e100bad51968c207f467f0d152a4a5
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-07-01 14:49:08 -0400

    updates

--- chem/sample_finetune.sh
+++ chem/sample_finetune.sh
@@ -1,1 +1,3 @@
 python finetune.py --split scaffold --epochs 0 --filename ${dataset}/${gnn_type}_nopretrain --gnn_type $gnn_type --dataset $dataset
+
+python finetune.py --model_file INPUT_MODEL_PATH --dataset DOWNSTREAM_DATASET --filename OUTPUT_FILE_PATH

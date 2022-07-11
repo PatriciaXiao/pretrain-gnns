c9adb9d93442255ee7ec3abe8c72aa918696ce09
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-07-10 20:45:04 -0700

    updates

--- chem/model.py
+++ chem/model.py
@@ -219,7 +219,7 @@ class GNN(torch.nn.Module):
         node representations
 
     """
-    def __init__(self, num_layer, emb_dim, JK = "last", drop_ratio = 0, gnn_type = "gin"):
+    def __init__(self, num_layer, emb_dim, JK = "last", drop_ratio = 0, gnn_type = "gin", prompting=True):
         super(GNN, self).__init__()
         self.num_layer = num_layer
         self.drop_ratio = drop_ratio
@@ -231,8 +231,11 @@ class GNN(torch.nn.Module):
         self.x_embedding1 = torch.nn.Embedding(num_atom_type, emb_dim)
         self.x_embedding2 = torch.nn.Embedding(num_chirality_tag, emb_dim)
 
-        max_prompt_size = 10 # TODO: I don't know where to find the maximum node number in the graphs?
-        self.prompt_embed = torch.nn.Embedding(max_prompt_size, emb_dim)
+        if prompting:
+            max_prompt_size = 10 # TODO: I don't know where to find the maximum node number in the graphs?
+            self.prompt_embed = torch.nn.Embedding(max_prompt_size, emb_dim)
+            self.x_embedding1.requires_grad = False
+            self.x_embedding2.requires_grad = False
 
         torch.nn.init.xavier_uniform_(self.x_embedding1.weight.data)
         torch.nn.init.xavier_uniform_(self.x_embedding2.weight.data)
@@ -265,6 +268,10 @@ class GNN(torch.nn.Module):
             raise ValueError("unmatched number of arguments.")
 
         x = self.x_embedding1(x[:,0]) + self.x_embedding2(x[:,1]) # the combination of two embedding parts makes the final embedding
+
+        import numpy as np
+        print(self.x_embedding1.weight[:,0])
+        input()
 
         h_list = [x]
         for layer in range(self.num_layer):

95dc291ba4a03ed20dd5c3327119ff3fa4a64259
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-07-12 01:09:27 -0700

    updates

--- chem/sample_finetune.sh
+++ chem/sample_finetune.sh
@@ -4,3 +4,5 @@ python finetune.py --epochs 1 --gnn_type gcn --input_model_file ./model_architec
 
 python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_supervised.pth --dataset tox21 --filename tox21/gcn_feature_prompt
 
+python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_supervised.pth --dataset hiv --filename hiv/gcn_feature_prompt
+

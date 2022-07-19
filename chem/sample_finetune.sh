038e74967479efd6ed1f576fcee61e52926214f0
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-07-18 17:28:01 -0700

    Update sample_finetune.sh

--- chem/sample_finetune.sh
+++ chem/sample_finetune.sh
@@ -10,3 +10,4 @@ python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_su
 
 python finetune.py --gnn_type gcn --dataset tox21 --filename tox21/gcn_feature_nopretrain -prompt none --eval_train
 
+python finetune.py --gnn_type gcn --dataset tox21 --filename tox21/gcn_feature_nopretrain --eval_train

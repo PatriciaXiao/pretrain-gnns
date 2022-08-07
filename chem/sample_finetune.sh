dc19c129b563c6c22a4b753bbf92fab3aa7cdca9
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-08-07 17:52:49 -0400

    random select doesn't work

--- chem/sample_finetune.sh
+++ chem/sample_finetune.sh
@@ -16,3 +16,6 @@ python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_su
 python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_supervised.pth --dataset tox21 --filename tox21/gcn_debug -prompt none --eval_train
 
 python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_supervised.pth --dataset tox21 --filename tox21/gcn_debug --eval_train --JK none
+
+
+python finetune.py --gnn_type gcn --input_model_file ./model_architecture/gcn_supervised.pth --dataset tox21 --filename tox21/gcn_debug --JK none
